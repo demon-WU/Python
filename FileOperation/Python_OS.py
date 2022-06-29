932eff051274474325bf81873d046d8dffa99708
demon <1653223198@qq.com>
2022-06-29 17:59:41 +0800

    文件的处理

---
+++ FileOperation/Python_OS.py
@@ -0,0 +1,44 @@
+import os
+import shutil
+
+# os.rename('test.txt','test(重命名).txt')
+'''
+重命名
+'''
+# os.remove('./test1(备份)txt')
+'''
+删除文件
+'''
+# os.mkdir('TestOS')
+'''
+创建文件夹:只能创建一级文件夹
+'''
+# os.rmdir('TestOS')
+'''
+删除文件夹
+'''
+# os.makedirs('d:/pubg/新建文件夹')
+'''
+创建多级目录
+'''
+# shutil.rmtree('d:/pubg')
+'''
+删除存在内容的文件夹
+'''
+# print(os.getcwd())
+'''
+获取当前目录
+'''
+# a=os.listdir("D:/")
+'''
+获得一个目录文件，以列表的形式存储
+'''
+# for i in a:
+#     print(i)
+#     pass
+with os.scandir('d:/') as ss:
+    for i in ss:
+        print(i)
+        pass
+    pass
+
